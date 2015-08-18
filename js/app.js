9679f02bb64daf59610b775100a8be39ccf8f668
Tyler Powers <gr4yscale@gmail.com>
2015-08-18 16:34:22 +0100

    update the tags selection list with possible tags when api client fetches data

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 define([
+  'underscore',
   'api_client',
   'models/VizModel',
   'viz',
-  'views/OverlayView'
-], function(ApiClient, VizModel, Viz, OverlayView){
+  'views/OverlayView',
+  'tag-selection'
+], function(_, ApiClient, VizModel, Viz, OverlayView, TagSelection){
 
   var init = function() {
   
@@ -15,10 +17,10 @@ define([
 
     ApiClient.fetchData(function(error, theData) {
 
-      var sampleCount = theData['sampleCount'];
+      var sampleCount = theData.sampleCount;
       var xOffset = -(sampleCount / 2);
       var xMax = vizModel.get('xScale') * sampleCount;
-      var tagCount = theData['tags'].length;
+      var tagCount = theData.tags.length;
       var zMax = (tagCount - 1) * vizModel.get('zScale');
 
       vizModel.set('timeSeriesData', theData);
@@ -27,9 +29,15 @@ define([
       vizModel.set('xMax', xMax);
       vizModel.set('tagCount', tagCount);
       vizModel.set('zMax', zMax);
-      vizModel.set('daysPerInterval', theData['daysPerInterval']);
+      vizModel.set('daysPerInterval', theData.daysPerInterval);
 
       viz.setupScene();
+
+      var possibleTags = _.map(theData.tags, function(tag) {
+        return { name : tag };
+      });
+
+      TagSelection.setPossibleTags(possibleTags);
     });
   };
 
--- js/tag-selection.js
+++ js/tag-selection.js
@@ -1,48 +1,68 @@
-var TagModel = Backbone.Model.extend({
-  defaults: {
-    id: null,
-    name: 'aTagName',
-    tagCount: null,
-    checked: 'checked'
-  }
+define([
+  'jquery',
+  'underscore',
+  'backbone'
+], function($, _, Backbone){
+
+  var TagModel = Backbone.Model.extend({
+    defaults: {
+      id:null,
+      name:'aTagName',
+      tagCount:null,
+      checked:'checked'
+    }
+  });
+
+  var TagsCollection = Backbone.Collection.extend({
+    model: TagModel
+  });
+
+  var TagsListItemView = Backbone.View.extend({
+    tagName: 'div',
+    className: 'tagView',
+    template: _.template($('#tag-list-item-tmpl').html()),
+
+    render: function() {
+      var html = this.template(this.model.toJSON());
+      this.$el.html(html);
+      return this;
+    }
+  });
+
+  var TagsListView = Backbone.View.extend({
+    el: '#tag-selection-overlay',
+
+    initialize: function() {
+      this.listenTo(this.collection, 'update', this.render);
+    },
+
+    render: function() {
+      var $list = this.$('div.tag-selection-list').empty();
+      var renderedItems = [];
+
+      this.collection.each(function(model) {
+        var item = new TagsListItemView({model: model});
+        renderedItems.push(item.render().$el);
+      }, this);
+
+      $list.append(renderedItems);
+
+      return this;
+    }
+  });
+
+  var possibleTags = new TagsCollection();
+  var selectedTags = new TagsCollection();
+
+  var tagsView = new TagsListView({collection: possibleTags});
+
+  var setPossibleTags = function(tagsData) {
+    possibleTags.set(tagsData);
+  };
+
+  return {
+    setPossibleTags : setPossibleTags,
+    possibleTags : possibleTags,
+    selectedTags : selectedTags
+  };
 });
-
-var TagsCollection = Backbone.Collection.extend({
-  model: TagModel
-});
-
-var TagsListItemView = Backbone.View.extend({
-  tagName: 'div',
-  className: 'tagView',
-  template: _.template($('#tag-list-item-tmpl').html()),
-
-  render: function() {
-    var html = this.template(this.model.toJSON());
-    this.$el.html(html);
-    return this;
-  }
-});
-
-var TagsListView = Backbone.View.extend({
-  el: '#tag-selection-overlay',
-
-  initialize: function() {
-    this.listenTo(this.collection, 'update', this.render);
-  },
-
-  render: function() {
-    var $list = this.$('div.tag-selection-list').empty();
-
-    this.collection.each(function(model) {
-      var item = new TagsListItemView({model: model});
-      $list.append(item.render().$el);
-    }, this);
-
-    return this;
-  }
-});
-
-var tagsList = new TagsCollection();
-var tagsView = new TagsListView({collection: tagsList});
-
-
